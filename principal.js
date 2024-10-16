1a043f001b5c303a024ba0fa3abf62ddb030d199
ketelyn-ket <ketelyn.cavalcante.santos@escola.pr.gov.br>
2024-10-16 10:46:28 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,6 @@
+var titulo = document.querySelector("titulo");
+titulo.textContent = "Aparecida Nuticionista";
+
+var paciente =document.querySelector("#primeiro-paciente");
+
+console.log(paciente);
