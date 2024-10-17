1a00b44415d40dd7e6af5f7d5ba4b59e0e857a09
ketelyn-ket <ketelyn.cavalcante.santos@escola.pr.gov.br>
2024-10-17 09:53:38 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,6 +1,15 @@
-var titulo = document.querySelector("titulo");
-titulo.textContent = "Aparecida Nuticionista";
+//var titulo = document.querySelector("titulo");
+//titulo.textContent = "Aparecida Nuticionista";
 
 var paciente =document.querySelector("#primeiro-paciente");
 
+
+var tdPeso  paciente.querySelector(".info-Peso");
+var Peso  tdPeso.textContent;
+
+var tdAltura  paciente.querySelector(".info-altura");
+var Altura  tdAltura.textContent;
+
 console.log(paciente);
+console.log(tdPeso);
+console.log(tdAltura);
