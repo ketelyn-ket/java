0c4bdc812f97af0825f8863770baa12f19527404
ketelyn-ket <ketelyn.cavalcante.santos@escola.pr.gov.br>
2024-10-22 08:12:07 -0300

    atualizacao

--- principal.js
+++ principal.js
@@ -1,15 +1,13 @@
 //var titulo = document.querySelector("titulo");
 //titulo.textContent = "Aparecida Nuticionista";
 
-var paciente =document.querySelector("#primeiro-paciente");
+var paciente = document.querySelector("#primeiro-paciente");
 
 
-var tdPeso  paciente.querySelector(".info-Peso");
-var Peso  tdPeso.textContent;
+var tdPeso = paciente.querySelector(".info-Peso");
+var Peso = tdPeso.textContent;
 
-var tdAltura  paciente.querySelector(".info-altura");
-var Altura  tdAltura.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdaltura.textContent;
 
-console.log(paciente);
-console.log(tdPeso);
-console.log(tdAltura);
+var imc = peso / (altura * altura);
