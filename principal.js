9dd7f2b9451004baaccef9c863cbb7fde81a61a6
ketelyn-ket <ketelyn.cavalcante.santos@escola.pr.gov.br>
2024-10-28 11:47:40 -0300

    alteracao

--- principal.js
+++ principal.js
@@ -3,11 +3,18 @@
 
 var paciente = document.querySelector("#primeiro-paciente");
 
-
 var tdPeso = paciente.querySelector(".info-Peso");
 var Peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdaltura.textContent;
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+if(peso< 0){
+    console.log("peso invalido");
+}
 
 var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
